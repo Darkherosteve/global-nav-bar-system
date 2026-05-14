6f7914f93eb81b1d295cf30871cbd366f2915d16
Steve <stevesonsaji10@gmail.com>
2026-05-14 19:58:07 +0530

    Fixed Index.html file for Public use

--- scripts/main.js
+++
@@ -1,7 +0,0 @@
-// Main JavaScript for navbar 
-// Minions Enterprises Solutions Lati System 
-console.log("Project: navbar loaded"); 
-document.addEventListener('DOMContentLoaded', function() { 
-    console.log("DOM fully loaded"); 
-    document.title = "navbar - Lati System"; 
-}); 
